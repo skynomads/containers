501905a99b35f118f0c587f6828325d37d6a193a
Jakob Gillich <jakob@gillich.me>
2022-09-16 11:58:17 +0200

    pocketbase: v0.7.3

--- pocketbase/entrypoint.sh
+++ pocketbase/entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -e
 
-if [ ! -f /var/pocketbase/data.sqlite ]; then
-	litestream restore -if-db-not-exists -if-replica-exists /var/pocketbase/data.db
-fi
+litestream restore -if-db-not-exists -if-replica-exists /var/pocketbase/data.db
 
 exec litestream replicate -exec "pocketbase --dir /var/pocketbase/ serve --http 0.0.0.0:8080"
