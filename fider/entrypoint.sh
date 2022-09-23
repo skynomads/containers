4eeccc73fa5ed99d123b8d347b47ee284d6155f1
Jakob Gillich <jakob@gillich.me>
2022-09-23 23:58:21 +0200

    add fider

---
+++ fider/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+
+./fider migrate
+
+./fider &
+PID=$!
+until $(curl --silent http://0.0.0.0:3000/); do
+    sleep 1
+done
+
+curl --silent -X POST http://localhost:3000/api/admins \
+  -H 'Content-Type: application/json' \
+  -d "{\"legalAgreement\":false,\"tenantName\":\"Feedback\",\"subdomain\":\"\",\"name\":\"User\",\"email\":\"$USER_EMAIL\"}"
+
+kill $PID
+
+exec ./fider
