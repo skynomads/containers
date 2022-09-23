b87b70da48e087515744da464ad748323c92e2c4
Jakob Gillich <jakob@gillich.me>
2022-09-24 00:16:01 +0200

    fider: fix entrypoint

--- fider/entrypoint.sh
+++ fider/entrypoint.sh
@@ -9,7 +9,7 @@ until $(curl --silent http://0.0.0.0:3000/); do
     sleep 1
 done
 
-curl --silent -X POST http://localhost:3000/api/admins \
+curl --silent -X POST http://localhost:3000/_api/tenants \
   -H 'Content-Type: application/json' \
   -d "{\"legalAgreement\":false,\"tenantName\":\"Feedback\",\"subdomain\":\"\",\"name\":\"User\",\"email\":\"$USER_EMAIL\"}"
 
