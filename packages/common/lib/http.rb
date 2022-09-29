d82421a066eafb07777206bc96ce864c19318fca
Jakob Gillich <jakob@gillich.me>
2022-09-29 22:14:22 +0000

    common: fix http request call

--- packages/common/lib/http.rb
+++ packages/common/lib/http.rb
@@ -12,8 +12,8 @@ require 'json'
 # end
 
 # retryable version of http.request
-def http_request(http, req, body = nil, retries = 5, &block)
-  http.request(req, body, block)
+def http_request(http, req, body = nil, retries = 5)
+  http.request(req, body)
 rescue StandardError => e
   raise(e) unless retries.positive?
 
@@ -24,7 +24,7 @@ end
 
 def post_json(url, data, header = {})
   uri = URI(url)
-  req = Net::HTTP::Post.new(uri.path, header.merge('Content-Type': 'application/json', 'Accept': 'application/json"'))
+  req = Net::HTTP::Post.new(uri.path, header.merge({ 'Content-Type': 'application/json', 'Accept': 'application/json"' }))
   req.body = data.to_json
   res = http_request(Net::HTTP.new(uri.host, uri.port), req)
   JSON.parse(res.body)
