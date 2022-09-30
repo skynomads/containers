3f4d3c3734a0739b0aa49a5481d02feb81f7a396
Jakob Gillich <jakob@gillich.me>
2022-09-30 00:40:13 +0000

    common: add patch_json

--- packages/common/lib/http.rb
+++ packages/common/lib/http.rb
@@ -1,15 +1,7 @@
 require 'net/http'
 require 'json'
 
-# def http_wait(url, tries = 5)
-#   Net::HTTP.get(url)
-# rescue StandardError => e
-#   raise(e) unless tries.positive?
-
-#   tries -= 1
-#   sleep 1
-#   retry
-# end
+JSON_HEADERS = { 'Content-Type': 'application/json', 'Accept': 'application/json"' }.freeze
 
 # retryable version of http.request
 def http_request(http, req, body = nil, retries = 5)
@@ -25,10 +17,20 @@ rescue StandardError => e
   retry
 end
 
-def post_json(url, data, header = {})
-  uri = URI(url)
-  req = Net::HTTP::Post.new(uri.path, header.merge({ 'Content-Type': 'application/json', 'Accept': 'application/json"' }))
+def json_request(uri, req, data)
   req.body = data.to_json
   res = http_request(Net::HTTP.new(uri.host, uri.port), req)
   JSON.parse(res.body)
+end
+
+def post_json(url, data, headers = {})
+  uri = URI(url)
+  req = Net::HTTP::Post.new(uri.path, headers.merge(JSON_HEADERS))
+  json_request(url, req, data)
+end
+
+def patch_json(url, data, headers = {})
+  uri = URI(url)
+  req = Net::HTTP::Patch.new(uri.path, headers.merge(JSON_HEADERS))
+  json_request(url, req, data)
 end
